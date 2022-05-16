c98b4da03048eedaa5d726b0ef0cfcdc38902b6d
Anuj Verma <anuj.verma20132@gmail.com>
2022-05-16 16:42:35 +0530

    some comments

--- app.py
+++ app.py
@@ -7,10 +7,14 @@ from sqlite3 import connect
 from csv import writer
 
 app = Flask(__name__)
-questions_status = {}
-test_data = {}
-recovery_data = {}
+questions_status = {}  # store data about each question while a test is live
+test_data = {}  # store data about the test in general while it is live
+recovery_data = {}  # store data about multiple parameters to use for recovery
+start_time = datetime.now()  # time of starting the test
+test_submitted = False  # flag to indicate if the test has been submitted
+last_known_time = datetime.now()  # time of the last interaction before error
 
+# template to create a new question
 NEW_QUESTION = {
     'number': 0,
     'visit': "unvisited",
@@ -21,42 +25,43 @@ NEW_QUESTION = {
     'subject': None
 }
 
+# subjects examined
 SUBJECTS = [
     'physics',
     'chemistry',
     'mathematics'
 ]
 
+# types of questions
 TYPES = [
     'mcq',
     'numeric'
 ]
 
+# main folder for a given user
 FOLDER = Path().home() / '.jee-prac'
 
-SCHEMA_PATH = Path(__file__).parent / 'schema.sql'
-DB_PATH = FOLDER / 'jee.db'
-RECOVERY_FILE = FOLDER / 'recovery.json'
-EXAMS_CSV_FILE = FOLDER / 'exams.csv'
-QUESTIONS_CSV_FILE = FOLDER / 'questions.csv'
+SCHEMA_PATH = Path(__file__).parent / 'schema.sql'  # path to the schema file
+DB_PATH = FOLDER / 'jee.db'  # path to the database
+RECOVERY_FILE = FOLDER / 'recovery.json'  # path to the recovery file
+EXAMS_CSV_FILE = FOLDER / 'exams.csv'  # path to the (temporary) exams csv file
+QUESTIONS_CSV_FILE = FOLDER / 'questions.csv'  # path to the (temporary) questions csv file
 
-START_TIME = datetime.now()
-LAST_KNOWN_TIME = datetime.now()
-SUBMITTED = False
-
-
+# setup the storage (files, folders, DB) for the application
 def setup_storage():
-    FOLDER.mkdir(exist_ok=True, parents=True)
+    FOLDER.mkdir(exist_ok=True, parents=True)  # create the folder if it doesn't exist
+
+    # create the DB from the schema if it doesn't exist and connect to it
     with connect(DB_PATH) as connection:
         with open(SCHEMA_PATH, 'r') as f:
             connection.executescript(f.read())
             connection.commit()
 
-
+# generate the list of questions for a given exam
 def produce_list_of_questions(exam_data: dict):
     questions = {}
-
     question_number = 1
+
     for subject_no, subject in enumerate(SUBJECTS, start=1):
         for type_no, type in enumerate(TYPES, start=1):
 
@@ -71,7 +76,7 @@ def produce_list_of_questions(exam_data: dict):
 
     return questions
 
-
+# count the number questions answered, unanswered, and marked
 def count_questions():
     counts = {
         'unanswered': 0,
@@ -91,20 +96,21 @@ def count_questions():
 
     return counts
 
-
+# save the recovery data to the recovery file
 def save_recovery_data():
-    global recovery_data, questions_status, LAST_KNOWN_TIME
+    global recovery_data, questions_status, last_known_time
 
-    LAST_KNOWN_TIME = datetime.now()
-    recovery_data['questions_status'] = questions_status
-    recovery_data['last_known_time'] = LAST_KNOWN_TIME.isoformat()
+    last_known_time = datetime.now()  # update the last known time
+    recovery_data['questions_status'] = questions_status  # update the questions status
+    recovery_data['last_known_time'] = last_known_time.isoformat()  # update the last known time
 
+    # write the recovery data to the recovery file
     with open(RECOVERY_FILE, 'w') as recovery:
         recovery.write(dumps(recovery_data))
 
-
+# check if the recovery data exists and if it does, load it
 def check_recovery_data():
-    global recovery_data, questions_status, test_data, START_TIME, LAST_KNOWN_TIME
+    global recovery_data, questions_status, test_data, start_time, last_known_time
 
     if RECOVERY_FILE.exists():
         with open(RECOVERY_FILE, 'r') as recovery:
@@ -114,74 +120,90 @@ def check_recovery_data():
                 recovery_data = loads(recovery_data)
                 questions_status = recovery_data['questions_status']
                 test_data = recovery_data['test_data']
-                LAST_KNOWN_TIME = datetime.fromisoformat(recovery_data['last_known_time'])
-                START_TIME = datetime.fromisoformat(recovery_data['start_time']) + (datetime.now() - LAST_KNOWN_TIME)
+                last_known_time = datetime.fromisoformat(recovery_data['last_known_time'])
+
+                # account for time loss due to crash, so that the candidate does not get penalized
+                start_time = datetime.fromisoformat(recovery_data['start_time']) + (datetime.now() - last_known_time)
 
                 return True
     return False
 
-
+# convert post-exam data to CSV
 def convert_to_csv(file_type: str):
+
+    # connect to the DB
     with connect(DB_PATH) as connection:
         cursor = connection.cursor()
     
+        # if the user is looking at the file that stores exam metadata
         if file_type == 'exams':
-
-            EXAMS_CSV_FILE.touch()
+            EXAMS_CSV_FILE.touch(exist_ok=True)  # create the file if it doesn't exist
 
             with open(EXAMS_CSV_FILE, 'w') as exams_csv:
 
                 exams_headers = ["exam_id", "start_time", "end_time"]
 
-                exams_data = cursor.execute(f"SELECT {', '.join(exams_headers)} FROM exams").fetchall()
+                exams_data = cursor.execute(f"SELECT {', '.join(exams_headers)} FROM exams").fetchall()  # get the data from the DB
                 
+                # write the data to the CSV file
                 exams_writer = writer(exams_csv)
                 exams_writer.writerow(exams_headers)
                 exams_writer.writerows(exams_data)
 
+        # if the user is looking at the file that stores questions
         elif file_type == 'questions':
                 
-                QUESTIONS_CSV_FILE.touch()
+                QUESTIONS_CSV_FILE.touch(exist_ok=True)  # create the file if it doesn't exist
     
                 with open(QUESTIONS_CSV_FILE, 'w') as questions_csv:
     
                     questions_headers = ["question_id", "question_number", "exam_id", "type", "subject", "value"]
     
-                    questions_data = cursor.execute(f"SELECT {', '.join(questions_headers)} FROM questions").fetchall()
+                    questions_data = cursor.execute(f"SELECT {', '.join(questions_headers)} FROM questions").fetchall()  # get the data from the DB
                     
+                    # write the data to the CSV file
                     questions_writer = writer(questions_csv)
                     questions_writer.writerow(questions_headers)
                     questions_writer.writerows(questions_data)
 
+        # if the user is looking at the file that does not exist
         else:
-            raise Exception(f"Invalid file type: {file_type}")
+            raise Exception(f"Invalid file type: {file_type}")  # yell at them
 
-
+# entry point for the application
+# this is where the user will configure a test and start it
 @app.route('/jee/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html'), HTTPStatus.OK
 
-
+# this is where the user will start the test
+# numbers of questions and time are received from a form submitted by the user
 @app.route('/jee/start', methods=['GET'])
 def start():
+
+    # if there are arguments in the URL
     if request.args:
-        global test_data, questions_status, START_TIME, recovery_data
+        global test_data, questions_status, start_time, recovery_data
+
+        # load the data into variables
         test_data = request.args.to_dict()
         questions_status = produce_list_of_questions(test_data)
-        START_TIME = datetime.now()
+        start_time = datetime.now()
 
+        # create the recovery file and write the data to it
         RECOVERY_FILE.touch()
         recovery_data = {
             'test_data': test_data,
-            'start_time': START_TIME.isoformat()
+            'start_time': start_time.isoformat()
         }
         save_recovery_data()
 
+        # redirect the user to the first question
         return redirect('/jee/question?number=1')
 
     return "", HTTPStatus.BAD_REQUEST
 
-
+# 
 @app.route('/jee/get_questions_status', methods=['GET'])
 def get_questions_status():
     global questions_status
@@ -279,7 +301,7 @@ def unmark():
 @app.route('/jee/submit', methods=['POST', 'GET'])
 def submit():
     END_TIME = datetime.now()
-    global questions_status, test_data, START_TIME, SUBMITTED
+    global questions_status, test_data, start_time, test_submitted
 
     if questions_status:
 
@@ -288,7 +310,7 @@ def submit():
 
             cursor.execute(
                 "INSERT INTO exams (start_time, end_time) VALUES (?, ?);",
-                (START_TIME, END_TIME)
+                (start_time, END_TIME)
             )
 
             connection.commit()
@@ -314,11 +336,11 @@ def submit():
 
             connection.commit()
 
-        SUBMITTED = True
+        test_submitted = True
 
         questions_status = {}
         test_data = {}
-        START_TIME = datetime.now()
+        start_time = datetime.now()
 
         RECOVERY_FILE.unlink(missing_ok=True)
 
@@ -328,11 +350,11 @@ def submit():
 
 @app.route('/jee/get_remaining_time', methods=['GET'])
 def get_remaining_time():
-    global START_TIME, test_data
+    global start_time, test_data
 
-    if START_TIME:
+    if start_time:
         remaining_time = (
-            int(test_data['duration']) * 60) - (datetime.now() - START_TIME).total_seconds()
+            int(test_data['duration']) * 60) - (datetime.now() - start_time).total_seconds()
 
         return dumps({'remaining_time': remaining_time}), HTTPStatus.OK
     return "", HTTPStatus.NOT_FOUND
@@ -351,24 +373,24 @@ def get_counts():
 
 @app.route('/jee/submitted', methods=['GET'])
 def submitted():
-    global SUBMITTED
+    global test_submitted
 
-    if SUBMITTED:
+    if test_submitted:
         return render_template('submitted.html'), HTTPStatus.OK
 
-    SUBMITTED = False
+    test_submitted = False
 
     return "", HTTPStatus.BAD_REQUEST
 
 
 @app.route('/jee/quit', methods=['GET'])
 def quit():
-    global questions_status, test_data, START_TIME
+    global questions_status, test_data, start_time
 
     if questions_status:
         questions_status = {}
         test_data = {}
-        START_TIME = datetime.now()
+        start_time = datetime.now()
 
         RECOVERY_FILE.unlink(missing_ok=True)
 
